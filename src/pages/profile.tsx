c24d43f6c30297f9161e4438adcc6a4eac78011c
CYPT71 <cyprientaib1@gmail.com>
2023-04-11 16:49:02 +0200

    beging user update

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,4 @@
-import { useGetUser } from '@/apis';
+import { useGetUser, useImageUpload } from '@/apis';
 import "@/styles/Profile.css"
 import { type User} from '@/types';
 function WebAuthn() {
@@ -8,6 +8,52 @@ function WebAuthn() {
   if (!!sessionStorage.token && !user) {
     useGetUser(sessionStorage.token, (c: any) => setUser(c))
   }
+
+  const openPopup = () => {
+    const popup = window.open("", "uploadPopup", "width=600,height=400");
+    if (popup) {
+      const channel = new BroadcastChannel("uploadChannel");
+
+      channel.onmessage = (event) => {
+        if (event.data.type === "image") {
+          const image = event.data.payload;
+          // Do something with the uploaded image here
+          useImageUpload(image, 3, (c: string)=>{
+            if (!!user) {
+              user.Icon = c
+              popup.close()
+            }
+          })
+        }
+      };
+
+      popup.document.write(`
+        <html>
+          <head>
+            <title>Upload Image</title>
+          </head>
+          <body>
+            <input type="file" accept="image/*" onchange="handleImageUpload(event)"/>
+            <script>
+              function handleImageUpload(event) {
+                const file = event.target.files[0];
+                const reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = () => {
+                  const image = reader.result;
+                  const channel = new BroadcastChannel("uploadChannel");
+                  channel.postMessage({
+                    type: "image",
+                    payload: image,
+                  });
+                };
+              }
+            </script>
+          </body>
+        </html>
+      `);
+    }
+  };
 
   return (
     <div style={{ marginBottom: 3 }}>
@@ -21,7 +67,9 @@ function WebAuthn() {
       ) : (
         <div className="profile-container">
           <div className="profile-header">
-            <img className="profile-icon" src={user?.Icon} alt="User icon" />
+            <img className="profile-icon" src={`http://127.0.0.1:3000/files?filename=${user?.Icon}`} alt="User icon" onClick={
+              (e) => openPopup()
+            }/>
             <h1 className="profile-username">{user?.Username}</h1>
             <p className="profile-email">{user?.Email}</p>
           </div>
