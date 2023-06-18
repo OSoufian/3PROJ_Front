031a6a9f21caad6ea3d21a243118d3aa27d9e199
OSoufian <osoufian@hotmail.fr>
2023-06-18 16:46:44 +0200

    Stylize channel videos page

--- src/pages/channel/[id].tsx
+++ src/pages/channel/[id].tsx
@@ -1,100 +1,108 @@
-import { useEditVideo, useGetUser, useGetMeChannel, useImageUpload, useGetVideosByChannel } from '@/apis';
-import "@/styles/EditVideo.css"
-import { type User, type VideoType, type ChannelType } from '@/types';
-
+import { type VideoType, type ChannelType } from "@/types";
+import { useGetChannelById, useGetVideosByChannel } from '@/apis';
+import "@/styles/ChannelVideosStyle.css"
 import { useParams } from 'react-router-dom';
 import envVars from "../../../public/env-vars.json"
 const baseURL = envVars["user-url"]
 
-function Channel() {
-  const [user, setUser] = useState<User | undefined>()
-  const [channel, setChannel] = useState<ChannelType | undefined>()
-  const [iconPath, setIconPath] = useState<string | null>()
-  const [currentVideo, setCurrentVideo] = useState<VideoType | undefined>()
-  const params = useParams();
-
-  useEffect(() => {
-    useGetMeChannel(sessionStorage.token ?? "", (c: ChannelType) => setChannel(c))
-  }, [])
-
-  useEffect(() => {
-    useGetVideosByChannel(channel?.Id, ["creation_date"], (c: VideoType[]) => {
-      const updatedVideos = c
-        .filter((v: VideoType) => v.Id == parseInt(params.id ?? ""))
-      setCurrentVideo(updatedVideos[0]);
-    });
-  }, [channel?.Id, params.id]);
-
-  useEffect(() => {
-    if (!!sessionStorage.token && !user) {
-      useGetUser(sessionStorage.token, (c: any) => setUser(c))
-    }
-  }, []);
-
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (currentVideo) {
-      const updatedVideo = { ...currentVideo, Name: e.target.value };
-      setCurrentVideo(updatedVideo);
-    }
-  };
-
-  const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (currentVideo) {
-      const updatedVideo = { ...currentVideo, Description: e.target.value };
-      setCurrentVideo(updatedVideo);
-    }
-  };
-
-  const handleIconChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (!!event.target.files && !!channel) {
-      useImageUpload(event.target.files[0], channel.OwnerId, (c: string) => {
-        setIconPath(c)
-        const updateVideo = { ...currentVideo, Icon: c } as VideoType;
-        setCurrentVideo(updateVideo);
-      });
-    }
-  };
-
-  return currentVideo ? (
-    <div>
-      <div className="input-div">
-        <h4 className="input-title">Name: </h4>
-        <input className="input" placeholder="Name" type="text" value={currentVideo.Name} onChange={handleNameChange} />
-      </div>
-      <div className="input-div">
-        <h4 className="input-title">Description: </h4>
-        <input className="input" placeholder="Description" type="text" value={currentVideo.Description} onChange={handleDescriptionChange} />
-      </div>
-      <div className="icon-div">
-        <h3 className="input-title">Icon</h3>
-        <img
-          className="video-thumbnail"
-          src={currentVideo.Icon ? `${baseURL}/image?imagename=${currentVideo.Icon}` : `${baseURL}/image?imagename=default.png`}
-          alt={currentVideo.Name}
-        />
-        <input id="icon-upload" type="file" accept="image/*" onChange={handleIconChange} style={{display:'none'}}/>
-        <div className="upload-icon">
-          <img src='https://cdn-icons-png.flaticon.com/512/126/126477.png' onClick={() => {
-            document.getElementById('icon-upload')?.click();
-          }}></img>
-        </div>
-      </div>
-
-
-      <Link to="/channel/">
-        <button className='save-btn' onClick={() => {
-          
-          if (!!iconPath) {
-            currentVideo.Icon = iconPath
-          }
-
-          useEditVideo(currentVideo, currentVideo.ChannelId, () => { })
-        }}>Save</button>  
-      </Link>
-    </div>
-  ) : (
-    <h1>No video</h1>
-  )
+interface Category {
+  id: number;
+  name: string;
 }
 
-export default Channel;
+const categories = [
+  {id: 1, name: "Videos"},
+  {id: 2, name: "Infos"}
+]
+
+
+function ChannelVideos() {
+    const [videoList, setVideoList] = useState<VideoType[] | null>()
+    const [channel, setChannel] = useState<ChannelType | undefined>();
+    const [activeCategory, setActiveCategory] = useState(1);
+    const param = useParams();
+
+    const handleCategoryClick = (category: Category) => {
+      setActiveCategory(category.id);
+    };
+
+    useEffect(() => {
+      useGetVideosByChannel(parseInt(param.id ?? ""),["created_at DESC"],(c: VideoType[]) => {
+        const updatedVideoList = c
+          .filter((v: VideoType) => v.ChannelId == parseInt(param.id ?? "") )
+          .map((v: VideoType) => {
+            let channel: ChannelType | undefined;
+  
+            handleRetreiveChannel(v, (c: ChannelType) => {
+              channel = c
+              setChannel(c)
+            })
+  
+            return {
+              ...v,
+              channelIcon: channel?.Icon
+            }
+  
+          })
+        setVideoList(updatedVideoList);
+      });
+    }, [parseInt(param.id ?? "")])
+
+    const handleRetreiveChannel = (video: VideoType, c: Function) => {
+        return useGetChannelById(video.ChannelId, c)
+    }
+    
+    return (
+      <div>
+        <div className="banner-section">
+          <img className="banner-image" src={`${baseURL}/image?imagename=${channel?.Banner}`} alt="Banner" />
+        </div>
+        <div className="profile-picture-section">
+          <img className="profile-picture" src={`${baseURL}/image?imagename=${channel?.Icon}`} alt="Profile Picture" />
+          <h2 className="channel-name">{channel?.Name}</h2>
+        </div>
+
+        <div className='panel'>
+      <div className='container'>
+      <ul>
+        {categories.map((category) => (
+          <Link key={category.id} to={``} className={`item${activeCategory === category.id ? ' active' : ''} dark:text-#C2C2C2`} onClick={() => handleCategoryClick(category)}>
+            {category.name}
+          </Link>
+        ))}
+        </ul>
+      </div>
+    </div>
+
+
+
+        <div className="video-list">
+          {activeCategory === 1 && !!videoList &&
+            videoList.map((v: VideoType) => (
+              <div key={v.Id} className="video-card">
+                <img
+                  className="w-full h-40 sm:h-52 object-cover"
+                  src={v.Icon ? `${baseURL}/image?imagename=${v.Icon}` : `${baseURL}/image?imagename=default.png`}
+                  alt={v.Name}
+                />
+                <div className="video-title">
+                  <h3>{v.Name}</h3>
+                </div>  
+                <div className="video-hidden"> <p>{v.IsHide ? "[Hidden]" : ""}</p></div>
+                <div className="video-hidden"> <p>{v.IsBlock ? "[Blocked]" : ""}</p></div>
+                <div className="video-description">
+                  <p>{v.Description}</p>
+                </div>                
+                <p>{`${v.Views} ${v.Views > 1 ? 'views' : 'view'}`}</p>
+                <p>{v.CreationDate}</p>
+                <Link to={`/watch/${v.Id}`} key={v.Id}>
+                  <button className="watch-btn">Watch Now</button>
+                </Link>
+              </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
+  
+  export default ChannelVideos;
